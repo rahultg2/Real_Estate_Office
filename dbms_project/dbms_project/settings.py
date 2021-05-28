d61a5a74208a6a42a0b1dfbed8beeda80fb58f87
rahultg2 <56204530+rahultg2@users.noreply.github.com>
2021-05-28 19:50:16 +0530

    Update settings.py

--- dbms_project/dbms_project/settings.py
+++ dbms_project/dbms_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')sn*r8&275(6b&976&77dz8)xgvi(@=gnvdim*2@=idjul*b_8'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
